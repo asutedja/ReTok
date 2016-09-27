29d70cfebbc81f6a9dc8b843784dc3d722f12395
Michael Chow <mcmcmc426@gmail.com>
2016-09-27 15:26:37 -0700

    passport auth, password hashing and checking completed

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -2,42 +2,54 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('../db/db').User;
 var session = require('express-session');
+var bcrypt = require('bcrypt');
+var Promise = require('bluebird');
 
-// configure strategy
-function verifyPassword(password, dbPassword) {
-	if (password === dbPassword) {
-		return true;
-	} else {
-		return false
-	}
-};
 
 passport.use(new LocalStrategy(
 	function(username, password, done) {
 	// search username and password for comparison
 		User.findAll({where: {username: username}})
 		.then(function(user) {
-			console.log('user: ', user);
 			if (user.length === 0) {return done(null, false, {message: 'wrong username'});}
-			if (!verifyPassword(password, user[0].password)) {return done(null, false, {message: 'wrong message'});}
+			if (!comparePw(password, user[0].password)) {return done(null, false, {message: 'wrong message'});}
 			return done(null, user);
 		});
 	}
 ));
 
 passport.serializeUser(function(user, done) {
-	console.log('user in serializeUser: ', user);
 	done(null, user[0].id);
 });
 
 passport.deserializeUser(function(id, done) {
-	console.log('id: ', id);
 	User.findAll({where: {id: id}})
 	.then(function(user) {
-		console.log("done", done);
 		done(null, user);
 	})
 	.catch(function(err){
 		done(err, null);
 	})
 });
+
+var comparePw = function (pw, hash) {
+	return bcrypt.compareSync(pw, hash);
+};
+
+var hashPw = function (pw, done) {
+	bcrypt.genSalt(10, function (err, salt) {
+		if (err) {
+			done(err, null);
+		} else {
+			bcrypt.hash(pw, salt, function (err, hash) {
+				if (err) {
+					done(err, null);
+				} else {
+					done(null, hash);
+				}
+			});
+		}
+	});
+};
+
+module.exports.hashPwAsync = Promise.promisify(hashPw);
--- server/server.js
+++ server/server.js
@@ -16,45 +16,6 @@ app.use(session({secret: 'lets ReTok'}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// // configure strategy
-// function verifyPassword(password, dbPassword) {
-// 	if (password === dbPassword) {
-// 		return true;
-// 	} else {
-// 		return false
-// 	}
-// };
-
-
-// passport.use(new LocalStrategy(
-// 	function(username, password, done) {
-// 	// search username and password for comparison
-// 		User.findAll({where: {username: username}})
-// 		.then(function(user) {
-// 			console.log('user: ', user);
-// 			if (user.length === 0) {return done(null, false, {message: 'wrong username'});}
-// 			if (!verifyPassword(password, user[0].password)) {return done(null, false, {message: 'wrong message'});}
-// 			return done(null, user);
-// 		});
-// 	}
-// ));
-
-// passport.serializeUser(function(user, done) {
-// 	console.log('user in serializeUser: ', user);
-// 	done(null, user[0].id);
-// });
-// passport.deserializeUser(function(id, done) {
-// 	console.log('id: ', id);
-// 	User.findAll({where: {id: id}})
-// 	.then(function(user) {
-// 		console.log("done", done);
-// 		done(null, user);
-// 	})
-// 	.catch(function(err){
-// 		done(err, null);
-// 	})
-// });
-
 app.use('/graphql', GraphHTTP({
 	schema: Schema,
 	pretty: true,
@@ -65,7 +26,6 @@ app.post('/login', passport.authenticate('local', {
 	successRedirect: '/',
 	failureRedirect: '/home',
 }) ,function(req, res) {
-	console.log('tried logged in');
 	res.status(200).send('welcome');
 	// res.redirect('/profile/' + req.user.username);
 });
