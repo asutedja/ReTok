6193b7e839a9f080f03995f53ccb6962f87c333a
Andrew Sutedja <andrew.sutedja@gmail.com>
2016-09-27 13:17:46 -0700

    Implement webRTC peer connection

--- server/server.js
+++ server/server.js
@@ -157,7 +157,6 @@ app.post('/login', passport.authenticate('local', {
 
 io.sockets.on('connection', function(socket) {
 
-
   // convenience function to log server messages on the client
   function log() {
     var array = ['Message from server:'];
@@ -165,7 +164,7 @@ io.sockets.on('connection', function(socket) {
     socket.emit('log', array);
   }
 
-  socket.on('connect', function(socket) {
+  socket.on('connection', function(socket) {
   	log('socket has connected');
   });
 
@@ -180,7 +179,7 @@ io.sockets.on('connection', function(socket) {
   socket.on('create or join', function(room) {
     log('Received request to create or join room ' + room);
 
-    var numClients = io.sockets.sockets.length;
+    var numClients = Object.keys(io.sockets.sockets).length;
     log('Room ' + room + ' now has ' + numClients + ' client(s)');
 
     if (numClients === 1) {
@@ -188,7 +187,7 @@ io.sockets.on('connection', function(socket) {
       log('Client ID ' + socket.id + ' created room ' + room);
       socket.emit('created', room, socket.id);
 
-    } else if (numClients === 2) {
+    } else if (numClients >= 2) {
       log('Client ID ' + socket.id + ' joined room ' + room);
       io.sockets.in(room).emit('join', room);
       socket.join(room);
