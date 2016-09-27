c02e4fad36f08a1f61af64e57088c17da8e2f831
Andrew Sutedja <andrew.sutedja@gmail.com>
2016-09-26 17:03:33 -0700

    Bring peerRTCConnection to our client unfunctionally

--- custom-preprocessor.js
+++ custom-preprocessor.js
@@ -4,10 +4,14 @@
 const babel = require('babel-core');
 const jestPreset = require('babel-preset-es2015');
 const react = require('babel-preset-react')
+<<<<<<< 2b9deea9e97b5afa3daaf6dc91fe8dcee63d566f
 <<<<<<< 11d24f975a084d4a77b6cdc5433e53d3f0fc47e2
 const stage = require('babel-preset-stage-1')
 =======
 >>>>>>> Rebase
+=======
+const stage = require('babel-preset-stage-1')
+>>>>>>> Bring peerRTCConnection to our client unfunctionally
 //const stage1 = require('webpack-dev-server')
 //const stage = require('webpack-preset')
 
@@ -16,11 +20,15 @@ module.exports = {
     if (babel.util.canCompile(filename)) {
       return babel.transform(src, {
         filename,
+<<<<<<< 2b9deea9e97b5afa3daaf6dc91fe8dcee63d566f
 <<<<<<< 11d24f975a084d4a77b6cdc5433e53d3f0fc47e2
         presets: [jestPreset, react, stage],
 =======
         presets: [jestPreset, react],
 >>>>>>> Rebase
+=======
+        presets: [jestPreset, react, stage],
+>>>>>>> Bring peerRTCConnection to our client unfunctionally
         retainLines: true,
       }).code;
     }
--- server/server.js
+++ server/server.js
@@ -20,7 +20,6 @@ var Schema = require('./db/Schema');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var os = require('os');
-
 var io = require('socket.io')(httpsServer);
  
 app.use(express.static('client'));
@@ -153,6 +152,64 @@ app.post('/login', passport.authenticate('local', {
 	res.status(200).send('welcome');
 	// res.redirect('/profile/' + req.user.username);
 });
+	
+
+
+
+io.sockets.on('connection', function(socket) {
+
+  // convenience function to log server messages on the client
+  function log() {
+    var array = ['Message from server:'];
+    array.push.apply(array, arguments);
+    socket.emit('log', array);
+  }
+
+  socket.on('message', function(message) {
+    log('Client said: ', message);
+    // for a real app, would be room-only (not broadcast)
+    socket.broadcast.emit('message', message);
+  });
+
+  socket.on('create or join', function(room) {
+    log('Received request to create or join room ' + room);
+
+    var numClients = io.sockets.sockets.length;
+    log('Room ' + room + ' now has ' + numClients + ' client(s)');
+
+    if (numClients === 1) {
+      socket.join(room);
+      log('Client ID ' + socket.id + ' created room ' + room);
+      socket.emit('created', room, socket.id);
+
+    } else if (numClients > 1) {
+      log('Client ID ' + socket.id + ' joined room ' + room);
+      io.sockets.in(room).emit('join', room);
+      socket.join(room);
+      socket.emit('joined', room, socket.id);
+      io.sockets.in(room).emit('ready');
+    } else { // max two clients
+      socket.emit('full', room);
+    }
+  });
+
+  socket.on('ipaddr', function() {
+    var ifaces = os.networkInterfaces();
+    for (var dev in ifaces) {
+      ifaces[dev].forEach(function(details) {
+        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+          socket.emit('ipaddr', details.address);
+        }
+      });
+    }
+  });
+
+  socket.on('bye', function(){
+    console.log('received bye');
+  });
+
+});
+
 
 http.listen(port, function(data) {
   console.log('listening on ' + port);
