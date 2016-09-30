185548858965a551178684172b8240d993ac72f0
Michael Chow <mcmcmc426@gmail.com>
2016-09-30 10:39:21 -0700

    remove duplicated login route in server.js

--- server/server.js
+++ server/server.js
@@ -45,45 +45,6 @@ app.use(/\/((?!graphql).)*/, bodyParser.json());
 
 // app.use(bodyparser.json());
 
-// // configure strategy
-// function verifyPassword(password, dbPassword) {
-// 	if (password === dbPassword) {
-// 		return true;
-// 	} else {
-// 		return false
-// 	}
-// };
-
-
-// passport.use(new LocalStrategy(
-// 	function(username, password, done) {
-// 	// search username and password for comparison
-// 		User.findAll({where: {username: username}})
-// 		.then(function(user) {
-// 			console.log('user: ', user);
-// 			if (user.length === 0) {return done(null, false, {message: 'wrong username'});}
-// 			if (!verifyPassword(password, user[0].password)) {return done(null, false, {message: 'wrong message'});}
-// 			return done(null, user);
-// 		});
-// 	}
-// ));
-
-// passport.serializeUser(function(user, done) {
-// 	console.log('user in serializeUser: ', user);
-// 	done(null, user[0].id);
-// });
-// passport.deserializeUser(function(id, done) {
-// 	console.log('id: ', id);
-// 	User.findAll({where: {id: id}})
-// 	.then(function(user) {
-// 		console.log("done", done);
-// 		done(null, user);
-// 	})
-// 	.catch(function(err){
-// 		done(err, null);
-// 	})
-// });
-
 app.use('/graphql', GraphHTTP({
   schema: Schema,
   pretty: true,
@@ -132,11 +93,6 @@ app.post('/login', passport.authenticate('local', {
      res.status(200).send(returnedData);
    });
  });
-});
-
-app.get('/logout', function (req, res){
-  req.logout();
-  res.redirect('/');
 });
 
 io.sockets.on('connection', function(socket) {
