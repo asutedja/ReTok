7f80c1e4c4220013855534646388c70df2c36fb2
EC2 Default User <ec2-user@ip-172-31-43-97.us-west-2.compute.internal>
2016-10-08 23:33:36 +0000

    Implement and deploy mySql DB and ReTok app

--- server/db/db.js
+++ server/db/db.js
@@ -2,8 +2,13 @@
 //CHANGE PASSWORD IN SETTINGS.JS IF NEEDED
 var Sequelize = require('sequelize');
 var userinfo = require('../../settings.js');
-var sequelize = new Sequelize('ReTok', userinfo.user, userinfo.password, {define: {charset: 'utf8mb4', collate: 'utf8mb4_unicode_ci'}});
 
+//var sequelize = new Sequelize('ReTok', userinfo.user, userinfo.password);
+var sequelize = new Sequelize(userinfo.connectionString, {  
+  dialectOptions: {
+    'SSL_VERIFY_SERVER_CERT': __dirname + '/amazon-rds-ca-cert.pem'
+  }
+});
 //define user model
 var User = sequelize.define('User', {
 	username: Sequelize.STRING,
