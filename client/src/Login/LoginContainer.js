8aa44b6b90bed6a6fc1c3f41f3d307025c5cdd9c
Andrew Sutedja <andrew.sutedja@gmail.com>
2016-10-11 11:55:36 -0700

    Attempt to add changes to main repo

--- client/src/Login/LoginContainer.js
+++ client/src/Login/LoginContainer.js
@@ -9,6 +9,10 @@ import * as userActions from '../Redux/userReducer'
 class LoginContainer extends React.Component {
 	constructor(props, context) {
 		super(props, context);
+		this.state = {
+			exist: false,
+			comma: false
+		}
 	}
 
 	componentWillMount() {
@@ -32,12 +36,7 @@ class LoginContainer extends React.Component {
 			comma: false
 		})
 		if(!user.includes(',') && !user.includes(':') && !user.includes('^') && !user.includes('#')) {
-
-			console.log('User ',user, ' Password ', password, 'firstName', firstName, 'lastName', lastName, 'email', email);
 			var userInfo = {username: user, password: password};
-
-
-
 			let myHeaders = new Headers({'Content-Type': 'application/graphql; charset=utf-8'});
 			let options = {
 
@@ -61,7 +60,7 @@ class LoginContainer extends React.Component {
 			fetch('/graphql', options).then((res) =>{
 				return res.json().then((data) => {
 					console.log(data);
-					if(data === null) {
+					if(data.data.addUser === null) {
 						this.setState({
 							exist: true
 						})
@@ -80,17 +79,21 @@ class LoginContainer extends React.Component {
 					}
 				})
 			})
+		} else {
+			this.setState({
+				comma: true
+			})
+		}
 
 
 
 	}
 
-
 	render() {
 
 		return(
 			<div>
-				<SignUpForm signUp={this.signUp.bind(this)}/>
+				<SignUpForm comma={this.state.comma} exist={this.state.exist} signUp={this.signUp.bind(this)}/>
 			</div>
 			)
 	}
