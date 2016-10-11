cd4948bffb20b5ea6b79b99bb2ace619f95dbf37
Robert Chao <robertkchao@gmail.com>
2016-10-11 10:17:25 -0700

    Refactors routes to use browser history

--- client/bundle.js
+++ client/bundle.js
@@ -35360,7 +35360,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var Routes = _react2.default.createElement(
   _reactRouter.Router,
-  { history: _reactRouter.hashHistory },
+  { history: _reactRouter.browserHistory },
   _react2.default.createElement(
     _reactRouter.Route,
     { path: '/', component: _HomeContainer2.default },
