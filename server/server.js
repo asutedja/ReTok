e5d0f5702c815b21e12c9db625e5ee03d7a86394
kukulo <yalo1986@gmail.com>
2016-10-11 11:11:24 -0700

    ready to implement securing routes

--- server/server.js
+++ server/server.js
@@ -59,8 +59,44 @@ app.use('/graphql', GraphHTTP({
   graphiql: true
 }));
 
+<<<<<<< 0c320f4ae08dc7a920ee0a7d1521bb00ffa2fa8c
 app.post('/login', passport.authenticate('local', {}) ,function(req, res) {
   var userID = req.session.passport.user;
+=======
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
+// app.post('/test', function(req, res) {
+// 	console.log('checking req body', req.body);
+// });
+
+// app.post('/login', passport.authenticate('local', {
+//   // successRedirect: '/',
+//   failureRedirect: '/',
+// }) ,function(req, res) {
+//   var userID = req.session.passport.user;
+//   console.log('checking my request over here -------->', req.session.passport.user);
+
+
+//   User.findAll({where:{id: userID}}).then(function(user) {
+//     console.log('confirming i have user information', user);
+//     res.status(200).send(user);
+//   });
+
+// });
+
+app.post('/login', passport.authenticate('local', {
+ // successRedirect: '/',
+ //failureRedirect: '/',
+}) ,function(req, res) {
+  console.log('this is cookie: ', req.session);
+  var userID = req.session.passport.user;
+  console.log('checking my request over here -------->', req.session);
+
+>>>>>>> ready to implement securing routes
   User.findAll({where:{id: userID}}).then(function(user) {
     var user0 = user[0];
     var resUser = [{
