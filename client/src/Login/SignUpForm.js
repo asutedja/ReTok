8322976d22b23786d0346ec31ef52b8b6897b798
robertkchao <robertkchao@gmail.com>
2016-10-11 11:35:44 -0700

    Revert "Implement emoji saving and redis client side"

--- client/src/Login/SignUpForm.js
+++ client/src/Login/SignUpForm.js
@@ -2,12 +2,10 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, browserHistory, IndexRoute, Link } from 'react-router'
 
-const SignUpForm = ({signUp, exist, comma}) => (
+const SignUpForm = (props) => (
   <div>
-    <form type='submit' className = "signUpForm" onSubmit={(e) => {e.preventDefault(); signUp(document.getElementById('newUser').value, document.getElementById('newPassword').value, document.getElementById('firstName').value, document.getElementById('lastName').value, document.getElementById('email').value);}}>
+    <form type='submit' className = "signUpForm" onSubmit={(e) => {e.preventDefault(); props.signUp(document.getElementById('newUser').value, document.getElementById('newPassword').value, document.getElementById('firstName').value, document.getElementById('lastName').value, document.getElementById('email').value);}}>
       <h2 id= "signUpHere">Sign Up Here</h2>
-      {exist ? <div>This username already exists!</div> : null}
-      {comma ? <div>Username cannot have any commas(,) or colons(:)</div> : null}
       <input id='newUser' className = "signUpInput" type='type/submit' placeholder='Username'/>
       <input id='newPassword' className = "signUpInput" type="password" placeholder='Password' />
       <input id='firstName' className = "signUpInput" type='type/submit' placeholder='First Names'/>
