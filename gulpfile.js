1905259d0a625d34782d86924b79c1eccfb1fef6
kukulo <yalo1986@gmail.com>
2016-10-11 11:11:24 -0700

    ready to implement securing routes

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,19 @@
 'use strict';
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+ 
+gulp.task('compress', function (cb) {
+  pump([
+        gulp.src('./client/bundle.js'),
+        uglify(),
+        gulp.dest('./client')
+    ],
+    cb
+  );
+});
+
 
 gulp.task('sass', function () {
   gulp.src('./client/styles/scss/*.scss')
