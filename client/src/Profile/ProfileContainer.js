79c9764799d00b992ac1190920f4705345442435
robertkchao <robertkchao@gmail.com>
2016-10-11 11:35:44 -0700

    Revert "Implement emoji saving and redis client side"

--- client/src/Profile/ProfileContainer.js
+++ client/src/Profile/ProfileContainer.js
@@ -34,7 +34,6 @@ class ProfileContainer extends React.Component {
     //Logic to update users friends as they login/logout and add new friends
     // socket.on('update', () => updateHelper(this))
     // updateHelper(this);
-    console.log(this.props.user);
   }
 
   goToUploadView() {
