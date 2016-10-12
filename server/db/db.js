b5d08b572170b6853655077bfcee4375606a6b21
Ubuntu <ubuntu@ip-172-31-39-228.us-west-2.compute.internal>
2016-10-12 18:40:42 +0000

    deploy ready

--- server/db/db.js
+++ server/db/db.js
@@ -2,8 +2,19 @@
 //CHANGE PASSWORD IN SETTINGS.JS IF NEEDED
 var Sequelize = require('sequelize');
 var userinfo = require('../../settings.js');
-var sequelize = new Sequelize('ReTok', userinfo.user, userinfo.password, {define: {charset: 'utf8mb4', collate: 'utf8mb4_unicode_ci'}});
 
+//var sequelize = new Sequelize('ReTok', userinfo.user, userinfo.password);
+var sequelize = new Sequelize('ReTok', userinfo.user, userinfo.password, {
+host: userinfo.connectionString,
+port: 3306,
+logging: console.log,
+maxConcurrentQueries: 100,
+dialectOptions: {
+ssl:'Amazon RDS'
+},
+pool: { maxConnections: 5, maxIdleTime: 30},
+language: 'en'
+})
 //define user model
 var User = sequelize.define('User', {
 	username: Sequelize.STRING,
