c5d0d18a825bc28e9c8d6466ed32e2c3b0b5050f
Andrew Sutedja <andrew.sutedja@gmail.com>
2016-10-12 18:56:04 -0700

    rebase

--- client/src/Nav/LoggedInNav/LoggedInNavContainer.js
+++ client/src/Nav/LoggedInNav/LoggedInNavContainer.js
@@ -62,8 +62,8 @@ class LoggedInNavContainer extends React.Component {
           socket.emit('updateFriends', this.props.friends);
           socket.disconnect()
           // console.log(data);
-        }.bind(this))
-    }.bind(this))
+        })
+    })
     .catch((error) => console.log(error))
 
     this.props.dispatch(userActions.sendSocket(null))
